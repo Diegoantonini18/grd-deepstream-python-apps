8e423d74a8766feaf0867b6aca90be38fbb3fbd1
Diego Antonini <dantonini@fi.uba.ar>
2024-02-29 18:54:23 -0300

    try improver rstp in rstp out

--- apps/deepstream-rtsp-in-rtsp-out_test/deepstream_test1_rtsp_in_rtsp_out.py
+++ apps/deepstream-rtsp-in-rtsp-out_test/deepstream_test1_rtsp_in_rtsp_out.py
@@ -34,76 +34,95 @@ import datetime
 
 import argparse
 
-MAX_DISPLAY_LEN = 64
 PGIE_CLASS_ID_VEHICLE = 0
 PGIE_CLASS_ID_BICYCLE = 1
 PGIE_CLASS_ID_PERSON = 2
 PGIE_CLASS_ID_ROADSIGN = 3
-MUXER_OUTPUT_WIDTH = 1920
-MUXER_OUTPUT_HEIGHT = 1080
 MUXER_BATCH_TIMEOUT_USEC = 33000
-TILED_OUTPUT_WIDTH = 1280
-TILED_OUTPUT_HEIGHT = 720
-GST_CAPS_FEATURES_NVMM = "memory:NVMM"
-OSD_PROCESS_MODE = 0
-OSD_DISPLAY_TEXT = 0
-pgie_classes_str = ["Vehicle", "TwoWheeler", "Person", "RoadSign"]
 
-# pgie_src_pad_buffer_probe  will extract metadata received on OSD sink pad
-# and update params for drawing rectangle, object information etc.
+def osd_sink_pad_buffer_probe(pad,info,u_data):
+    frame_number=0
+    #Intiallizing object counter with 0.
+    obj_counter = {
+        PGIE_CLASS_ID_VEHICLE:0,
+        PGIE_CLASS_ID_PERSON:0,
+        PGIE_CLASS_ID_BICYCLE:0,
+        PGIE_CLASS_ID_ROADSIGN:0
+    }
+    num_rects=0
 
-
-def pgie_src_pad_buffer_probe(pad, info, u_data):
-    frame_number = 0
-    num_rects = 0
     gst_buffer = info.get_buffer()
     if not gst_buffer:
         print("Unable to get GstBuffer ")
         return
 
     # Retrieve batch metadata from the gst_buffer
+    # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
+    # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
     batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
     l_frame = batch_meta.frame_meta_list
     while l_frame is not None:
         try:
+            # Note that l_frame.data needs a cast to pyds.NvDsFrameMeta
+            # The casting is done by pyds.NvDsFrameMeta.cast()
+            # The casting also keeps ownership of the underlying memory
+            # in the C code, so the Python garbage collector will leave
+            # it alone.
             frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
         except StopIteration:
             break
 
-        frame_number = frame_meta.frame_num
+        frame_number=frame_meta.frame_num
         num_rects = frame_meta.num_obj_meta
-        print("\nFrame Number =", frame_number, "Number of Objects =", num_rects)
-
-        # Iterating through object metadata in frame
-        l_obj = frame_meta.obj_meta_list
+        l_obj=frame_meta.obj_meta_list
         while l_obj is not None:
             try:
-                obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
+                # Casting l_obj.data to pyds.NvDsObjectMeta
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            obj_counter[obj_meta.class_id] += 1
+            try: 
+                l_obj=l_obj.next
             except StopIteration:
                 break
 
-            # Get class id and then the class name
-            class_id = obj_meta.class_id
-            class_name = pgie_classes_str[class_id]
+        # Acquiring a display meta object. The memory ownership remains in
+        # the C code so downstream plugins can still access it. Otherwise
+        # the garbage collector will claim it when this probe function exits.
+        display_meta=pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+        display_meta.num_labels = 1
+        py_nvosd_text_params = display_meta.text_params[0]
+        # Setting display text to be shown on screen
+        # Note that the pyds module allocates a buffer for the string, and the
+        # memory will not be claimed by the garbage collector.
+        # Reading the display_text field here will return the C address of the
+        # allocated string. Use pyds.get_string() to get the string content.
+        py_nvosd_text_params.display_text = "Frame Number={} Number of Objects={} Vehicle_count={} Person_count={}".format(frame_number, num_rects, obj_counter[PGIE_CLASS_ID_VEHICLE], obj_counter[PGIE_CLASS_ID_PERSON])
 
-            print("Detected Object: ", class_name)
+        # Now set the offsets where the string should appear
+        py_nvosd_text_params.x_offset = 10
+        py_nvosd_text_params.y_offset = 12
 
-            try:
-                l_obj = l_obj.next
-            except StopIteration:
-                break
+        # Font , font-color and font-size
+        py_nvosd_text_params.font_params.font_name = "Serif"
+        py_nvosd_text_params.font_params.font_size = 10
+        # set(red, green, blue, alpha); set to White
+        py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
 
-        if ts_from_rtsp:
-            ts = frame_meta.ntp_timestamp / 1000000000
-            print("RTSP Timestamp:", datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-
+        # Text background color
+        py_nvosd_text_params.set_bg_clr = 1
+        # set(red, green, blue, alpha); set to Black
+        py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
+        # Using pyds.get_string() to get display_text as string
+        print(pyds.get_string(py_nvosd_text_params.display_text))
+        pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
         try:
-            l_frame = l_frame.next
+            l_frame=l_frame.next
         except StopIteration:
             break
-
+			
     return Gst.PadProbeReturn.OK
-
 
 
 def cb_newpad(decodebin, decoder_src_pad, data):
